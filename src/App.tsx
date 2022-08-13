4a8adf3323b5d58a468a63c7f0e7e52c8524866f
ori winboim <yanuka7@gmail.com>
2022-08-14 01:41:51 +0300

    fixed android full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import {
-  IonApp,
-  setupIonicReact,
-} from "@ionic/react";
+import { IonApp, setupIonicReact } from "@ionic/react";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -22,15 +19,34 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+import { StatusBar } from "@capacitor/status-bar";
 import Routes from "./Routes";
-
+import { useEffect } from "react";
+import { AndroidFullScreen } from "@awesome-cordova-plugins/android-full-screen";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <Routes/>
-  </IonApp>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    const hideStatusBar = async () => {
+      await StatusBar.hide();
+    };
+    const fullScreen = async () => {
+      if (await AndroidFullScreen.isImmersiveModeSupported())
+        await AndroidFullScreen.immersiveMode();
+      else {
+        await AndroidFullScreen.showUnderSystemUI();
+      }
+    };
+    hideStatusBar();
+    fullScreen();
+  }, []);
+
+  return (
+    <IonApp>
+      <Routes />
+    </IonApp>
+  );
+};
 
 export default App;
